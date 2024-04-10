c6dd6683e8505971f0da91b3f9e906516eaf7d8b
JuanSandalinas <146134950+JuanSandalinas@users.noreply.github.com>
2024-04-10 14:40:24 +0200

    More errrors

--- advanced dataset/missing_data.py
+++ advanced dataset/missing_data.py
@@ -31,19 +31,22 @@ class Missing_data():
             - df_or: Copy of the original Dataframe
         """
         
-        df = pd.read_csv('C:/Users/joann/OneDrive/Escritorio/Intro/DMT-1/dataset_mood_smartphone.csv')
+        df = pd.read_csv('../DMT-1/dataset_mood_smartphone.csv')
         df = df.drop('Unnamed: 0', axis = 1)
-        df[['day', 'time']] = df['time'].str.split(' ', expand=True)
+        df['date'] = pd.to_datetime(df['time'])
+        
         self.df = df
         self.df_or = df.copy()
         self.nan_var = None
         self.nan_count = None
+
         self.ids = self.df['id'].unique() 
+        self.time = self.df['time'].unique() 
+        self.variables = self.df['variable'].unique() 
 
 
     def nan_finder(self):
         """ 
-
         Given the data frame, returns the variables with NaN values and the number of Nan Values
         """
         
@@ -75,25 +78,27 @@ class Missing_data():
         miss_df.nan_finder()
         miss_df.df.loc[miss_df.nan_indices[0],'value'] = 0
 
-    def plotter(self,var):
+    def plot(self,var):
         """
         PLots the evolution of a dataset overtime of one variable.
         Inputs:
             - Variable: Variable to plot
         """
+
+        plt.figure(figsize=(10, 6))
         for id_ in self.ids:
             
             data_id = self.df[self.df['id'] == id_]
             
-            plt.figure(figsize=(10, 6))
-            plt.plot(data_id['time'], data_id[data_id['variable'] == var]['values'], label=f'ID {id_}')
+
+            plt.plot(data_id[data_id['variable'] == var]['date'], data_id[data_id['variable'] == var]['value'], label=f'ID {id_}')
     
-            plt.title(f'{var} Over Time (ID {id_})')
-            plt.xlabel('Time')
-            plt.ylabel('Values')
-            plt.legend()
-            plt.grid(True)
-            plt.show()
+        plt.title(f'{var} Over Time')
+        plt.xlabel('Time')
+        plt.ylabel('Values')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
             
 
     def reset(self):
@@ -101,7 +106,6 @@ class Missing_data():
 
         Resets the current dataframe to its original form, without filled missed values
         """
-
 
         self.df = self.df_or.copy()
 
@@ -134,9 +138,11 @@ if __name__ == '__main__':
  
 
     miss_df = Missing_data(df)
-    miss_df.nan_finder()
 
     miss_df.moving_average()
+
+    print(miss_df.df)
+    miss_df.plot('circumplex.arousal')
 
     
 
