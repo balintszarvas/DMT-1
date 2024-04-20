05aa41a8d843c3e997d358505812e4e5dc2db998
JuanSandalinas <146134950+JuanSandalinas@users.noreply.github.com>
2024-04-20 18:15:47 +0200

    Residuals ploits inside linear regression file

--- advanced dataset/models/linear_regression.py
+++ advanced dataset/models/linear_regression.py
@@ -8,12 +8,14 @@ from sklearn.linear_model import Ridge
 from sklearn.model_selection import KFold
 from sklearn.model_selection import LeaveOneOut
 import matplotlib.pyplot as plt
+import scipy.stats as stats
+
 
 
 import pandas as pd
 
 
-
+### BASIS FUNCTIONS
 
 def polynomial(X, degree = 3):
 
@@ -32,20 +34,7 @@ def polynomial(X, degree = 3):
 
     poly_features = PolynomialFeatures(degree=degree)
 
-    return poly_features.fit_transform(X_train)
-
-def gaussian(X):
-    """
-    Computes the data in gaussian activation
-    """
-    return None
-    
-def logistic_sigmoid(X):
-    """
-    Computes the data with logistic sigmoid
-    """
-    return None
-
+    return poly_features.fit_transform(X)
 
 def linear(X):
     """
@@ -54,7 +43,19 @@ def linear(X):
     return X
 
 
+def gaussian(X):
+    """
+    Computes teh data with gaussian transformations. NOT YET IMPLEMENTED, ONLY IF LINEAR OR PLYNOMAIL DOES NOT WORK.
+    """
+    return None
+    
+def logistic_sigmoid(X):
+    """
+    Computes the data with logistic sigmoid. NOT YET IMPLEMENTED, ONLY IF LINEAR OR PLYNOMAIL DOES NOT WORK.
+    """
+    return None
 
+ ### SUPPORT FUNCTIONS
 def linear_regression(X_train, y_train,ridge = False, **kwargs):
 
     """ Does linear regression with a specific degree
@@ -131,28 +132,37 @@ def compute_accuracy(y_pred, y_test):
 
 
 
+### MODEL TRAINING
 
-def train_model(df_train,df_test, target,ridge = False, basis = linear, k_fold = 5, alpha = 1):
+def train_model(df, target,ridge = False, basis = linear, k_fold = 5, alpha = 1):
 
-    """Train a specified model
+    """Train a linear regression model. This is the relevant functions, the other ones are support function
     
 
-    Inputs:
+    Parameters:
+    -----------
 
     df: Dataframe. Note, Id column will be dropped
 
     target: Target column
-    model: type of function to use in the linear regression
 
-    k-fold : Fraction of the data used for training.
+    model: type of function to use in the linear regression. Can be linear, gaussian, polynomial or logistic
+
+    k-fold : Fraction of the data used for training, if false uses LOOCV
+
+    alpha: For ridge regression, lamba in Overleaf
 
 
     Returns:
+    -----------
 
-    accuracy: accuracy fo the model 
+    accuracy: accuracy of the model all trained models 
     model: training model
+    mean_accuracy: The mean accuracy of all models
+    y_pred,y_test: last prediction and test done
+
     """
-    X, y = df.drop(target, axis = 1), df_train[target]
+    X, y = df.drop(target, axis = 1), df[target]
     accuracy = []
     
     if k_fold == False:
@@ -171,29 +181,39 @@ def train_model(df_train,df_test, target,ridge = False, basis = linear, k_fold =
         accuracy.append(compute_accuracy(y_pred, y_test))
     
 
-    return model, accuracy, np.mean(accuracy)
+    return model, accuracy, np.mean(accuracy), y_pred, y_test
 
-def plotter(parameters, mean_accuracies):
-    plt.plot(parameters,mean_accuracies)
-    plt.xlabel("")
-    plt.ylabel("k_fold")
+def plot_residuals(y_pred, y_test, compare = True):
+    """Does the residuals plots:
+    Parameters:
+    -----------
+    y_pred: predicted data
+    y_test:actual data
+    """
+    
+    residuals = y - y_pred
+
+
+    plt.figure(figsize=(8, 6))
+    plt.hist(residuals, bins=10, density=True, alpha=0.6, color='blue', edgecolor='black')
+    if compare == True:
+        mu, sigma = np.mean(residuals), np.std(residuals)
+        xmin, xmax = plt.xlim()
+        x = np.linspace(xmin, xmax, 100)
+        p = stats.norm.pdf(x, mu, sigma)
+        plt.plot(x, p, 'k', linewidth=2)
+        plt.title('Residuals Histogram and Normal Distribution')
+        plt.xlabel('Residuals (Actual - Predicted)')
+        plt.ylabel('Density')
+        plt.legend(['Normal Distribution', 'Residuals'])
+
     plt.grid(True)
     plt.show()
-
-def plot_predictions(y_pred, y):
-
-    fig, axs = plt.subplots(1,1)
-    axs.plot(y_pred, label = "Predicted", color = 'blue')
-    axs.plot(y, label = "Original", color = 'green')
-    plt.title(compute_accuracy(y_pred, y))
-    axs.grid(True)
-    plt.show()
-
 
 
 if __name__ == '__main__':
     
-    df = pd.read_csv('../DMT-1/advanced dataset/data/train.csv')
+    df = pd.read_csv('../DMT-1/train.csv')
     columns_to_keep = ['mood','activity', 'appCat.other', 'appCat.social', 'circumplex.valence']
 
 
@@ -202,8 +222,10 @@ if __name__ == '__main__':
     df = df.drop(columns=columns_to_drop)
     k_fold = 5
 
-    model, accuracy, mean_accuracy,y_pred,y = train_model(df_train, df_test, target='mood', k_fold = False)
-    plot_predictions(y_pred,y)
+    model, accuracy, mean_accuracy,y_pred,y_test = train_model(df, target='mood', k_fold = 10)
+    print(y_pred)
+    print(y_test)
+    plot_residuals(y_pred,y_test)
 
 
 
